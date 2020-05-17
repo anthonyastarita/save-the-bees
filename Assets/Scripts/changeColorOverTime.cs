9e1c72921b6352c6bf3899b1ef6b975f0da7b36b
Jonan <jjsalomonzon@gmail.com>
2020-05-17 17:58:40 -0400

    leaves and bees

---
+++ Assets/Scripts/changeColorOverTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColorOverTime : MonoBehaviour
+{
+
+    SpriteRenderer leafSprite;
+    public float lerpTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leafSprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(ColorLerp());
+    }
+
+
+
+
+    IEnumerator ColorLerp()
+    {
+
+        float startTime = Time.time;
+        float percentageComplete = 0;
+
+        while (percentageComplete < 1)
+        {
+            float elapsedTime = Time.time - startTime;
+            percentageComplete = elapsedTime / (lerpTime);
+            leafSprite.color = Color.Lerp(Color.green, Color.red, percentageComplete);
+            yield return null;
+        }
+    }
+
+}
