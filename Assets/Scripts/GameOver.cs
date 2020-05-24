50cf0fc073191f7b9811a808fe23bc2c9ebedcf3
Jonan <jjsalomonzon@gmail.com>
2020-05-24 14:19:26 -0400

    game over added

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private BeeManager beeManager;
+
+    /// <summary>
+    /// The amount of bees required to pass on to the next level. Can be changed to whatever.
+    /// </summary>
+    private int BEE_REQUIREMENT => 1 * 1 * 1 * 1 * 1 * 1;
+
+    //Subscribe to this event from another class to do something when the game is over
+    public event Action OnGameOver;
+
+    //Subscribe to other class events to customize when we want the game to end
+    private void Awake()
+    {
+        beeManager.OnCurrentBeeCountChanged += (beeCount) =>
+        {
+            if (beeCount < BEE_REQUIREMENT) EndGame();
+        };
+    }
+
+    //Use this method to end the game
+    private void EndGame()
+    {
+        OnGameOver?.Invoke();
+    }
+}
---
+++ Assets/Scripts/GameOverView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameOverView : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject display;
+
+    private void Awake()
+    {
+        var gameOver = GetComponent<GameOver>();
+        gameOver.OnGameOver += () => Display();
+    }
+
+    private void Display(bool show = true)
+    {
+        display.gameObject.SetActive(show);
+    }
+}
