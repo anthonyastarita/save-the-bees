2b1b96295a8ac62dfd9b754ee656ec747ff06356
Anthony Astarita <anthonyastarita98@gmail.com>
2020-05-25 22:07:30 -0400

    added more bee facts

--- Assets/Scripts/BeeFactDisplay.cs
+++ Assets/Scripts/BeeFactDisplay.cs
@@ -15,7 +15,15 @@ public class BeeFactDisplay : MonoBehaviour
     {
         "Honey bees must gather nectar from two million flowers to make one pound of honey.",
         "One bee has to fly about 90,000 miles – three times around the globe – to make one pound of honey.",
-        "The average bee will make only 1/12th of a teaspoon of honey in its lifetime."
+        "The average bee will make only 1/12th of a teaspoon of honey in its lifetime.",
+        "The bees’ buzz is the sound made by their wings which beat 11,400 times per minute.",
+        "The honey bee is the only insect that produces food eaten by people",
+        "Male honey bees have no stinger and do no work.",
+        "A colony of bees consists of 20,000-60,000 honey bees and one queen.",
+        "Honey bees communicate with one another by dancing.",
+        "A honey bee visits 50 to 100 flowers during a collection trip.",
+        "Bees have 5 eyes and 6 legs.",
+
     };
 
     private void Awake()
